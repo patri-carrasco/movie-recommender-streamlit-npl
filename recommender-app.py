6e32f85dcf6b423786a313bd4b5eadf4d4d917c4
patri-carrasco <patri.carrasco@gmail.com>
2021-04-22 12:45:41 +0200

    Creat user_input_features

--- recommender-app.py
+++ recommender-app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+from src.user_input_features import user_input_features
+
+
 
 st.write("""
 # Movier recommender using NPL
 
 """)
+df = pd.read_csv("./data/data_best_ratings.csv")
 
 
 image = Image.open('./images/film.jpg')
@@ -17,3 +21,7 @@ st.sidebar.header('Title')
 
 title = st.sidebar.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
+
+film =user_input_features(title,df)
+st.write(film)
+
---
+++ src/user_input_features.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+
+def user_input_features(title, df):
+
+    film = df.loc[df['original_title']== title]
+    data = {'original_title': film.original_title,
+            'year': film.year,
+            'genre': film.genre,
+            'director': film.director,
+            'avg_vote':film.avg_vote,
+            'votes':film.votes
+            }
+    features = pd.DataFrame(data)
+    return features
