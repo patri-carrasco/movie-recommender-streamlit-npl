07b7f904fcdd7a83dd5d01c3c40841f83b09ec9a
patri-carrasco <patri.carrasco@gmail.com>
2021-05-13 12:41:57 +0200

    errores

--- recommender-app.py
+++ recommender-app.py
@@ -5,6 +5,9 @@ from PIL import Image
 from src.user_input_features import user_input_features
 from src.npl import get_recommendations
 
+st.set_page_config(page_title='Movier recommender',
+    layout='wide')
+
 #Título de la aplicación web
 st.write("""
 # Movier recommender using NPL
@@ -24,12 +27,18 @@ title = st.sidebar.text_input('Movie title', 'Life of Brian')
 #Título de los datos indtroducidos por el usuario
 st.write('The current movie title is', title)
 film =user_input_features(title,df)
-st.write(film)
+
+if len(film)!=0:
+    st.write(film)
 
 
-# Título del recomendador
-st.write('Recommendations offered by the description of the chosen film the npl')
+    # Título del recomendador
+    st.write('Recommendations offered by the description of the chosen film the npl')
 
-recommender = pd.DataFrame(get_recommendations(title,df))
+    recommender = pd.DataFrame(get_recommendations(title,df))
 
-st.write(recommender)
+    st.write(recommender)
+else:
+    st.write("""
+        # The title was not found in the database, please try again
+        """)
